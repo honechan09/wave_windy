ee70a6d8be3d75b6fba6055511239d5e7034a41c
[honechan09] <[yms8991@gmail.com]>
2025-06-13 21:54:42 +0900

    フロントエンドにvue,バックエンドにgo,echo

--- main.go
+++ main.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"io"
 	"log"
-	"net/http"
-	"os"
 
-	"wave_windy/openweather_api"
+	"wave_windy/routes"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Echo!")
-	})
-	e.Logger.Fatal(e.Start(":8080"))
-	//ichinomiya, chiba
-	lat := 35.377426
-	lon := 140.390991
-	//openweather API key
-	//https://openweathermap.org/current
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(".envファイルの読み込みに失敗しました")
 	}
-	apiKey := os.Getenv("API_KEY")
-	// 5days par 3hours
-	result, err := openweather_api.GetWeather(lat, lon, apiKey)
-	if err != nil {
-		log.Fatalf("API取得失敗: %v", err)
+
+	e := echo.New()
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
 	}
-	fmt.Println(result)
+	e.Renderer = renderer
+
+	routes.Init(e)
+	e.Logger.Fatal(e.Start(":8080"))
 }
