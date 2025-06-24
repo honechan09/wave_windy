138dda2537d1798a9f168408c1ce4c48bf8f11d0
honechan9 <yms8991@]gmail.com>
2025-06-24 23:16:18 +0900

    リポジトリの削除をしたため、復旧中

--- main.go
+++ main.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "log"
+    "net/http"
+    "os"
 
-	"wave_windy/openweather_api"
+    "wave_windy/openweather_api"
 
-	"github.com/joho/godotenv"
-	"github.com/labstack/echo/v4"
+    "github.com/joho/godotenv"
+    "github.com/labstack/echo/v4"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Echo!")
-	})
-	e.Logger.Fatal(e.Start(":8080"))
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal(".envファイルの読み込みに失敗しました")
+    }
 	//ichinomiya, chiba
-	lat := 35.377426
-	lon := 140.390991
-	//openweather API key
-	//https://openweathermap.org/current
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(".envファイルの読み込みに失敗しました")
-	}
 	apiKey := os.Getenv("API_KEY")
-	// 5days par 3hours
-	result, err := openweather_api.GetWeather(lat, lon, apiKey)
-	if err != nil {
-		log.Fatalf("API取得失敗: %v", err)
-	}
-	fmt.Println(result)
+    lat := 35.377426
+    lon := 140.390991
+	//openweather API key
+
+	e := echo.New()
+    e.GET("/", func(c echo.Context) error {
+        result, err := openweather_api.GetWeather(lat, lon, apiKey)
+        if err != nil {
+            return c.String(http.StatusInternalServerError, "API取得失敗: "+err.Error())
+        }
+        return c.String(http.StatusOK, result)
+    })
+    e.Logger.Fatal(e.Start(":8080"))
+	log.Println("サーバーが起動しました: http://localhost:8080")
+
 }
