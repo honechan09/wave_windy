bc3f9d5c44d6a550880e1d7ab2158c90e5b63582
honechan9 <yms8991@]gmail.com>
2025-06-25 23:26:08 +0900

    うねりの取得

--- main.go
+++ main.go
@@ -1,36 +1,66 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "os"
+	"log"
+	"net/http"
+	"os"
 
-    "wave_windy/openweather_api"
+	"wave_windy/openweather_api"
 
-    "github.com/joho/godotenv"
-    "github.com/labstack/echo/v4"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal(".envファイルの読み込みに失敗しました")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(".envファイルの読み込みに失敗しました")
+	}
 	//ichinomiya, chiba
-	apiKey := os.Getenv("API_KEY")
-    lat := 35.377426
-    lon := 140.390991
+	apiKey := os.Getenv("API_KEY_WINDY")
+	lat := 35.377426
+	lon := 140.390991
 	//openweather API key
 
 	e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        result, err := openweather_api.GetWeather(lat, lon, apiKey)
-        if err != nil {
-            return c.String(http.StatusInternalServerError, "API取得失敗: "+err.Error())
-        }
-        return c.String(http.StatusOK, result)
-    })
-    e.Logger.Fatal(e.Start(":8080"))
+	e.GET("/", func(c echo.Context) error {
+		result_windy, err_windy := openweather_api.GetWeather(lat, lon, apiKey)
+		result_surge, err_surge := openweather_api.GetSurgeWether(lat, lon)
+		if err_windy != nil {
+			return c.String(http.StatusInternalServerError, "API取得失敗: "+err_windy.Error())
+		}
+
+		if err_surge != nil {
+			return c.String(http.StatusInternalServerError, "API取得失敗: "+err_surge.Error())
+
+		}
+		result := result_surge + "\n" + result_windy
+		if result == "" {
+			return c.String(http.StatusInternalServerError, "結果が不足しています "+err_surge.Error())
+		}
+
+		return c.String(http.StatusOK, result)
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 	log.Println("サーバーが起動しました: http://localhost:8080")
 
 }
+
+// うねりのAPI
+// import (
+// 	"fmt"
+// 	"wave_windy/openweather_api"
+// )
+
+// func main() {
+// 	// 必要な引数をセット
+// 	lat := 35.377426
+// 	lon := 140.390991
+
+// 	result, err := openweather_api.GetSurgeWether(lat, lon)
+// 	if err != nil {
+// 		fmt.Println("エラー:", err)
+// 		return
+// 	}
+// 	fmt.Println(result)
+// }
--- openweather_api/surge_api.go
+++ openweather_api/surge_api.go
@@ -1,3 +1,26 @@
 // うねりのAPI
 // https://open-meteo.co
 package openweather_api
+
+import (
+	// "encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func GetSurgeWether(lat, lon float64) (string, error) {
+	url := fmt.Sprintf("https://marine-api.open-meteo.com/v1/marine?latitude=%f&longitude=%f&current=swell_wave_height&hourly=swell_wave_height,swell_wave_direction&timezone=Asia%%2FTokyo", lat, lon)
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
